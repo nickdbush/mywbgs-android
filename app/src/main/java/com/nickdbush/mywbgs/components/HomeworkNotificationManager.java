603425d80482be9be75c66364493f3efbf88ef64
Nicholas Bush <nickdbush@hotmail.co.uk>
2016-10-31 00:23:18 +0000

    Moved homework notifications to service

--- app/src/main/java/com/nickdbush/mywbgs/components/HomeworkNotificationManager.java
+++ app/src/main/java/com/nickdbush/mywbgs/components/HomeworkNotificationManager.java
@@ -1,6 +1,7 @@
 package com.nickdbush.mywbgs.components;
 
 import android.app.AlarmManager;
+import android.app.IntentService;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
@@ -48,63 +49,80 @@ public class HomeworkNotificationManager extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Realm realm = Realm.getDefaultInstance();
-        RealmResults<Homework> homeworks = realm.where(Homework.class)
-                .lessThanOrEqualTo("dueDate", new LocalDate().plusDays(1).toDate())
-                .equalTo("completed", false)
-                .findAll()
-                // TODO: 30/10/2016 Test this
-                .sort("dueDate", Sort.ASCENDING, "period", Sort.ASCENDING);
-
-        String title;
-        String text = "";
-        List<String> subjects = new ArrayList<String>();
-
-        int count = 0;
-        if (homeworks.size() > 0) {
-            for (int i = 0; i < homeworks.size(); i++) {
-                Homework homework = homeworks.get(i);
-                count++;
-                if (subjects.contains(homework.getLesson().getSubject())) continue;
-                boolean isOverdue = homeworks.get(i).getDueDate().toLocalDateTime(new LocalTime(8, 30)).isBefore(new LocalDateTime());
-                subjects.add(homework.getLesson().getSubject() + (isOverdue ? " (overdue)" : ""));
-            }
-        } else {
-            return;
-        }
-
-        for (int i = 0; i < subjects.size(); i++) {
-            if (i + 1 < subjects.size()) {
-                text += subjects.get(i) + (i + 2 < subjects.size() ? ", " : " ");
-            } else if (subjects.size() > 1) {
-                text += "and " + subjects.get(i);
-            } else {
-                text = subjects.get(i);
-            }
-        }
-
-        title = count + " piece" + (count > 1 ? "s" : "") + " of homework " + (count > 1 ? "need" : "needs") + " to be done for tomorrow";
-
-        // Create the notification and display
-        Intent notifyIntent = new Intent(context, HomeworkActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, NOTIFICATION_ID, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        Notification notification = new Notification.Builder(context)
-                .setContentTitle(title)
-                .setContentText(text)
-                .setContentIntent(pendingIntent)
-                .setSmallIcon(R.drawable.ic_silhouette)
-                .build();
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            notification.color = ContextCompat.getColor(context, R.color.colorPrimaryDark);
-        }
-
-        // Auto-close on click
-        notification.flags |= Notification.FLAG_AUTO_CANCEL;
-
-        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(context);
-        managerCompat.notify(NOTIFICATION_ID, notification);
-        realm.close();
+        Intent intentService = new Intent(context, HomeworkNotificationService.class);
+        context.startService(intentService);
     }
+
+    public static class HomeworkNotificationService extends IntentService {
+        public HomeworkNotificationService() {
+            super("HomeworkNotificationManager");
+        }
+
+        @Override
+        protected void onHandleIntent(Intent intent) {
+            Realm realm = Realm.getDefaultInstance();
+            try {
+                RealmResults<Homework> homeworks = realm.where(Homework.class)
+                        .lessThanOrEqualTo("dueDate", new LocalDate().plusDays(1).toDate())
+                        .equalTo("completed", false)
+                        .findAll()
+                        // TODO: 30/10/2016 Test this
+                        .sort("dueDate", Sort.ASCENDING, "period", Sort.ASCENDING);
+
+                String title;
+                String text = "";
+                List<String> subjects = new ArrayList<String>();
+
+                int count = 0;
+                if (homeworks.size() > 0) {
+                    for (int i = 0; i < homeworks.size(); i++) {
+                        Homework homework = homeworks.get(i);
+                        count++;
+                        if (subjects.contains(homework.getLesson().getSubject())) continue;
+                        boolean isOverdue = homeworks.get(i).getDueDate().toLocalDateTime(new LocalTime(8, 30)).isBefore(new LocalDateTime());
+                        subjects.add(homework.getLesson().getSubject() + (isOverdue ? " (overdue)" : ""));
+                    }
+                } else {
+                    return;
+                }
+
+                for (int i = 0; i < subjects.size(); i++) {
+                    if (i + 1 < subjects.size()) {
+                        text += subjects.get(i) + (i + 2 < subjects.size() ? ", " : " ");
+                    } else if (subjects.size() > 1) {
+                        text += "and " + subjects.get(i);
+                    } else {
+                        text = subjects.get(i);
+                    }
+                }
+
+                title = count + " piece" + (count > 1 ? "s" : "") + " of homework " + (count > 1 ? "need" : "needs") + " to be done for tomorrow";
+
+                // Create the notification and display
+                Intent notifyIntent = new Intent(this, HomeworkActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(this, NOTIFICATION_ID, notifyIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+                Notification notification = new Notification.Builder(this)
+                        .setContentTitle(title)
+                        .setContentText(text)
+                        .setContentIntent(pendingIntent)
+                        .setSmallIcon(R.drawable.ic_silhouette)
+                        .build();
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    notification.color = ContextCompat.getColor(this, R.color.colorPrimaryDark);
+                }
+
+                // Auto-close on click
+                notification.flags |= Notification.FLAG_AUTO_CANCEL;
+
+                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+                managerCompat.notify(NOTIFICATION_ID, notification);
+            } finally {
+                realm.close();
+                stopSelf();
+            }
+        }
+    }
+
 }
